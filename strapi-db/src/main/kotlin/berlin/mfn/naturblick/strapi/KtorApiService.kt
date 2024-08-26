ceb3cf271a76592e6b484536bd9b02963279cfb4
localadmin <omid.khorramshahi@mfn.berlin>
2024-08-26 17:30:43 +0200

    changed data source of strapi-db-plugin from strapi to ktor-service

--- strapi-db/src/main/kotlin/berlin/mfn/naturblick/strapi/KtorApiService.kt
+++ strapi-db/src/main/kotlin/berlin/mfn/naturblick/strapi/KtorApiService.kt
@@ -5,26 +5,35 @@
 
 package berlin.mfn.naturblick.strapi
 
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType
 import okhttp3.OkHttpClient
 import okhttp3.ResponseBody
 import retrofit2.Retrofit
 import retrofit2.http.GET
 import java.util.concurrent.TimeUnit
 
+private val contentType = MediaType.get("application/json")
 
 private val client = OkHttpClient.Builder()
     .readTimeout(90, TimeUnit.SECONDS)
     .build()
 
 interface KtorApiService {
-    @GET("ktor-strapidb")
+
+    @GET("app-content/db")
     suspend fun getFile(): ResponseBody
+
+    @GET("app-content/character-values")
+    suspend fun getCharacterValues(): List<CharacterValue>
 }
 
 object KtorApi {
     fun service(baseUrl: String): KtorApiService {
         val retrofit = Retrofit.Builder()
             .baseUrl(baseUrl)
+            .addConverterFactory(Json { ignoreUnknownKeys = true }.asConverterFactory(contentType))
             .client(client)
             .build()
         return retrofit.create(KtorApiService::class.java)
