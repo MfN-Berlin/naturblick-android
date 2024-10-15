aadbb729bca51a9787a602002d15a78bca9aa77a
Petter Arvidsson <petter.arvidsson@mfn.berlin>
2024-10-15 10:22:37 +0200

    Add device and app info to feedback email

--- app/src/main/java/berlin/mfn/naturblick/ui/info/feedback/FeedbackFragment.kt
+++ app/src/main/java/berlin/mfn/naturblick/ui/info/feedback/FeedbackFragment.kt
@@ -7,13 +7,17 @@ package berlin.mfn.naturblick.ui.info.feedback
 
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import berlin.mfn.naturblick.BuildConfig
+import berlin.mfn.naturblick.R
 import berlin.mfn.naturblick.databinding.FragmentFeedbackBinding
 import berlin.mfn.naturblick.utils.setSingleClickListener
+import de.cketti.mailto.EmailIntentBuilder
 
 class FeedbackFragment : Fragment() {
     override fun onCreateView(
@@ -23,9 +27,11 @@ class FeedbackFragment : Fragment() {
     ): View {
         val binding = FragmentFeedbackBinding.inflate(inflater, container, false)
         binding.feedbackEmail.setSingleClickListener {
-            startActivity(
-                Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:naturblick@mfn.berlin"))
-            )
+            EmailIntentBuilder.from(requireActivity())
+                .to("naturblick@mfn.berlin")
+                .subject(requireContext().resources.getString(R.string.feedback_subject, BuildConfig.VERSION_NAME))
+                .body(requireContext().resources.getString(R.string.feedback_body, Build.MODEL, Build.VERSION.RELEASE, BuildConfig.VERSION_NAME))
+                .start()
         }
         return binding.root
     }
