52f3c63711b02380152d1eeaec63e14539c41baa
Petter Arvidsson <petter.arvidsson@mfn.berlin>
2025-07-16 10:32:50 +0200

    Fix insets for compose in fieldbook
    - Scaffold manages insets for FAB and content
    - Top bar manages its own insets

--- app/src/main/java/berlin/mfn/naturblick/ui/fieldbook/fieldbook/FieldbookActivity.kt
+++ app/src/main/java/berlin/mfn/naturblick/ui/fieldbook/fieldbook/FieldbookActivity.kt
@@ -15,11 +15,13 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.material.AlertDialog
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
@@ -211,6 +213,7 @@ class FieldbookActivity : FragmentActivity() {
 
             NaturblickTheme {
                 Scaffold(
+                    contentWindowInsets = WindowInsets.systemBars,
                     topBar = {
                         TopBar(
                             selectionCount = selection.size,
@@ -408,6 +411,7 @@ class FieldbookActivity : FragmentActivity() {
         var search by remember { mutableStateOf(false) }
         val isInSelectionMode = selectionCount > 0
         TopAppBar(
+            windowInsets = WindowInsets.systemBars,
             navigationIcon = {
                 NavigationIcon {
                     if (!search && !isInSelectionMode) {
